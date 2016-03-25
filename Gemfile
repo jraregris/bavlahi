6f38299f2ad7bac04b91553a25511b4e07d08d2a
jraregris <oddmund@oddmundo.com>
2016-03-25 21:09:48 +0100

    Add Coveralls

--- Gemfile
+++ Gemfile
@@ -2,5 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rake', '~> 11.1'
 gem 'dotenv', '~> 2.1'
-gem 'minitest', '~> 5.8', '>= 5.8.4'
 
+
+gem 'minitest', '~> 5.8', '>= 5.8.4'
+gem 'coveralls', require: false
--- test/saver_test.rb
+++ test/saver_test.rb
@@ -1,13 +1,14 @@
 require 'minitest/autorun'
 
-require './lib/saver'
+require 'test/test_helper'
+
+require 'lib/saver'
 
 describe Saver do
   it "should write the given object to the save file" do
     testIO = StringIO.new
 
     Saver.save_all({ test: 'testen'}, testIO)
-
     testIO.string.must_equal "---\n:test: testen\n"
   end
 end
